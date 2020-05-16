20d1f92d6f00daeeda260515f666b8508be1c47d
Logan Negley <logannegley@gmail.com>
2020-05-15 20:00:46 -0500

    auth middleware done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -21,7 +21,7 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  
 });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,20 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+module.exports = (req,res,next) =>{
+  const token = req.headers.authorization;
+
+  if(token){
+    jwt.verify(token, secret.jwtSecret, (error, decodedToken) =>{
+      if(error){
+        res.status(401).json({message:'bad authorization'})
+      }else{
+        req.decodedToken = decodedToken;
+        next()
+      }
+    })
+    }else{
+      res.status(401).json({message:'bad authorization'})
+    }
+}
