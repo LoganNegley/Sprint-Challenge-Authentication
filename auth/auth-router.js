a853e130a28570969469b61218dbafd0b0580ae9
Logan Negley <logannegley@gmail.com>
2020-05-15 20:08:16 -0500

    login endpoint done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -21,7 +21,27 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  
+  const {username, password} = req.body;
+
+  db.findBy({username})
+  .then(user =>{
+    if(user && bcrypt.compareSync(password, user.password)){
+      const token = generateToken(user)
+
+      res.status(200).json({
+        message: `Welcom ${user.username}!`,
+        token: token
+      });
+    }else{
+      res.status(401).json({message:'Invalid credentials'})
+    }
+  })
+  .catch(error =>{
+    console.log(error)
+    res.status(500).json({
+      errorMessage: 'Error with server'
+    })
+  })
 });
 
 function generateToken(user){
